47c01e697ad58db8141d1d26d0bec12748b8a205
ajit283 <55892788+ajit283@users.noreply.github.com>
2022-07-26 23:00:16 +0200

    Initial Commit

---
+++ keyboardremapper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  keyboardremapper
+//
+//  Created by Ajit Mistry on 26.07.22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
